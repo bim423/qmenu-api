e3f39348f401426a6262f6e627657952cad02d9f
burakmutluay <mburakmutluay@gmail.com>
2021-01-15 18:28:16 +0300

    Personnel domain is implemented

--- src/main/java/com/qrmenu/domain/Personnel.java
+++ src/main/java/com/qrmenu/domain/Personnel.java
@@ -3,23 +3,33 @@ package com.qrmenu.domain;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @RequiredArgsConstructor
 @Entity
+@Table(name="personnel")
 public class Personnel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="personnel_id")
     private int id;
 
-    private String firstName;
-    private String lastName;
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
     private String email;
-    private int role;
+
+    @Column(name = "admin")
+    private int admin;
 }
