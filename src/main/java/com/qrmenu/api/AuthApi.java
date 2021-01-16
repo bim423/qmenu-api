b4ce5614a83b5821559aa8936afc158dd0f3ff4c
umutalacam <umutcanalacam@gmail.com>
2021-01-16 17:39:56 +0300

    Extend auth API response

--- src/main/java/com/qrmenu/api/AuthApi.java
+++ src/main/java/com/qrmenu/api/AuthApi.java
@@ -4,13 +4,13 @@ import com.qrmenu.data.PersonnelRepository;
 import com.qrmenu.domain.Personnel;
 import com.qrmenu.service.Message;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -20,26 +20,29 @@ public class AuthApi {
     @Autowired
     PersonnelRepository repo;
 
-    @PutMapping(consumes = "application/json")
-    public ResponseEntity<Message> auth(@RequestBody Personnel personnel){
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<?> auth(@RequestBody Personnel personnel){
 
         Optional<Personnel> optional = repo.findByUsername(personnel.getUsername());
 
         if (!optional.isPresent()){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new Message("Username not found", 0));
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new Message("Invalid username.", 0));
         }
 
         Personnel check = optional.get();
 
         if (check.getPassword().equals(personnel.getPassword())){
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(new Message("Login successful" , 0));
-        }else {
+            HashMap<String, Object> responseMap = new HashMap<>();
+            responseMap.put("message", "Login Successful");
+            responseMap.put("user", check);
+
+            return new ResponseEntity<Object>(responseMap, HttpStatus.OK);
+
+        } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body(new Message("Invalid password", 0));
         }
-
 
     }
 }
